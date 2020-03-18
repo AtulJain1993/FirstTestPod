327d858b92caa6e1f80258b42ec635a23082488c
Atul Apporio <atul@apporio.com>
2020-03-18 13:18:40 +0530

    Added test cases

---
+++ FirstTestPod/FirstTestPod.swift
@@ -0,0 +1,21 @@
+//
+//  FirstTestPod.swift
+//  FirstTestPod
+//
+//  Created by Atul Jain on 18/03/20.
+//  Copyright © 2020 Atul Jain. All rights reserved.
+//
+
+public final class FirstTestPod {
+    
+    let name = "FirstTestPod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
